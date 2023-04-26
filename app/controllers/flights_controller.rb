7df88555d8bb39db59a4d8499610290ee3498c8d
brickwall <sk292207@gmail.com>
2023-04-27 00:40:23 +0530

    todays commit

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,12 @@
 class FlightsController < ApplicationController
 	def index
 		@airports = Airport.all
-		if params['commit']
-   			@flight_list = Flight.includes(:arrival_airport, :departure_airport)
+		if params['commit']	
+   			all_flights = Flight.includes(:arrival_airport, :departure_airport, :aircraft)
+   			@available_flights = all_flights.select do |flight|
+			   flight.arrival_airport.code == params[:to] and flight.departure_airport.code == params[:from] and\
+ 					flight.datetime > params[:flight][:date] and flight.datetime < params[:flight][:date].to_date+1
+ 			end
 		end
 	end
 end
